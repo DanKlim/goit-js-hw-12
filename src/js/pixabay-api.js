43dd88dc9399b46b345703f83035507d5a19064e
Danylo <danylo.klymkevych@gmail.com>
2025-06-04 07:58:54 +0300

    complete

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const API_KEY = '50595861-e3300a20d14d5dbbfabcd1d5e';
+
+export async function getImagesByQuery(query, page) {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 15,
+        page,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log('Помилка при запиті:', error);
+    throw error;
+  }
+}
+
+// export function getImagesByQuery(query) {
+//   return axios
+//     .get('https://pixabay.com/api/', {
+//       params: {
+//         key: API_KEY,
+//         q: query,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//     })
+//     .then(response => {
+//       return response.data;
+//     })
+//     .catch(error => {
+//       console.log('Помилка при запиті:', error);
+//       throw error;
+//     });
+// }
